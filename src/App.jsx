dd78b4ffd4a4abe30e5e0502c50afe2920f3388f
VicRmzChz <vicentermzchz@gmail.com>
2022-04-13 23:46:33 -0500

    Creating Navbar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navbar } from "./components/Navbar";
+//import Main from "./components/Main";
+
+export function App() {
+    return (
+        <Navbar />
+    );
+}
